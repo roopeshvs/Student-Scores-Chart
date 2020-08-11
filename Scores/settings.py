4ad2b94a5d14a2d404c8f220675ce1bdfd4cc651
Roopesh V S <txtmeroopesh@gmail.com>
2020-08-11 13:34:14 +0530

    CORS Fix #2

--- Scores/settings.py
+++ Scores/settings.py
@@ -111,7 +111,7 @@ try:
         if url.scheme == 'mysql':
             DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
 except Exception:
-    print 'Unexpected error:', sys.exc_info()
+    print('Unexpected error:', sys.exc_info())
 
 
 # Password validation
